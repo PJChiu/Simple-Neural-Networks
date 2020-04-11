ced74de935f93fe1211b4bd40c887ce8c794975d
Ricardo <54463077+PJChiu@users.noreply.github.com>
2020-04-11 15:28:24 -0500

    Update grdescent.py

--- grdescent.py
+++ grdescent.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  7 19:03:09 2019
-
-@author: Jerry Xing
-"""
 import numpy as np
 def grdescent(func, w0, stepsize, maxiter, tolerance = 1e-2):
 #% function [w]=grdescent(func,w0,stepsize,maxiter,tolerance)
